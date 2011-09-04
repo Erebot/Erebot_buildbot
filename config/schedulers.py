37185ca2471f2a9c1047134c08a7140edaced437
François Poirotte <clicky@erebot.net>
2011-09-04 16:55:52 +0200

    Run the tests on WinXP for every change.

--- config/schedulers.py
+++ config/schedulers.py
@@ -10,25 +10,11 @@ SCHEDULERS = [
         builderNames=[
             'Tests - Debian 6 - PHP 5.2',
             'Tests - Debian 6 - PHP 5.3',
+            'Tests - WinXP - PHP 5.3',
             'Packaging',
             'Quality Assurance',
             'API documentation',
         ],
-    ),
-
-    # It costs more to run the tests on Windows,
-    # so we run the tests on it less often.
-    timed.Nightly(
-        name="Windows",
-        branch="master",
-        builderNames=[
-            'Tests - WinXP - PHP 5.3',
-        ],
-        # Run the tests once a week on Mondays, at 5:00am.
-        hour=5,
-        minute=0,
-        dayOfWeek=0,
-        onlyIfChanged=True,
     ),
 ]
 
