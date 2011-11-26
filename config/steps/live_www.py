8fb5696109a54f50870380eb9e37aeaf409b789d
François Poirotte <clicky@erebot.net>
2011-11-26 19:03:30 +0100

    Fix path issue.

--- config/steps/live_www.py
+++ config/steps/live_www.py
@@ -5,13 +5,19 @@ from Erebot_buildbot.src import master
 
 LIVE_WWW = factory.BuildFactory()
 LIVE_WWW.addStep(master.MasterShellCommand(
-    command="cd /var/www/Erebot/erebot && git pull",
+    command=" && ".join([
+        "cd /var/www/Erebot/erebot"
+        "git pull"
+    ]),
     description=["Updating"],
     descriptionDone=["Updated"],
 ))
 
 LIVE_WWW.addStep(master.MasterShellCommand(
-    command="git log -1 --pretty=oneline --no-color | cut -d ' ' -f 1",
+    command=" && ".join([
+        "cd /var/www/Erebot/erebot",
+        "git log -1 --pretty=oneline --no-color | cut -d ' ' -f 1"
+    ]),
     description=["Revision"],
     descriptionDone=["Revision"],
     extract_fn=lambda rc, stdout, stderr: {'got_revision': stdout},
