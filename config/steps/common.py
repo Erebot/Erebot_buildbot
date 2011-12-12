38f37b0aafa87eb491d82737a12ae7fbb1f17546
François Poirotte <clicky@erebot.net>
2011-12-12 23:26:50 +0100

    Last mistake ?

--- config/steps/common.py
+++ config/steps/common.py
@@ -16,9 +16,8 @@ def convert_repourl(repository):
     return 'git@%s:%s.git' % tuple(repository.split('://', 1)[1].split('/', 1))
 
 def _extract_repository(rc, stdout, stderr):
-    return {
-        "rw_repository": convert_repourl(stdout.strip()),
-    }
+    repos = stdout.strip().rpartition('.git')[0]
+    return {"rw_repository": convert_repourl(repos)}
 
 clone = Git(
     mode='clobber',
