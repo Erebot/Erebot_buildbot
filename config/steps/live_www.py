3fd96e1841abd8b2f8b4c7725127f7b1f0d429a7
François Poirotte <clicky@erebot.net>
2012-02-26 17:20:27 +0100

    Switch paster directories.

--- config/steps/live_www.py
+++ config/steps/live_www.py
@@ -6,8 +6,8 @@ from Erebot_buildbot.src import master
 LIVE_WWW = factory.BuildFactory()
 LIVE_WWW.addStep(master.MasterShellCommand(
     command=" && ".join([
-        "cd /var/www/Erebot/erebot",
-        "/usr/bin/git pull",
+        "cd /var/www/www.erebot.net/",
+        "/usr/bin/git pull origin master",
     ]),
     description=["Updating"],
     descriptionDone=["Updated"],
@@ -15,7 +15,7 @@ LIVE_WWW.addStep(master.MasterShellCommand(
 
 LIVE_WWW.addStep(master.MasterShellCommand(
     command=" && ".join([
-        "cd /var/www/Erebot/erebot",
+        "cd /var/www/www.erebot.net/",
         "/usr/bin/git log -1 --pretty=oneline --no-color | cut -d ' ' -f 1",
     ]),
     description=["Revision"],
