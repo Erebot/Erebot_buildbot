dfc0a95d6adbc4eeee9d650a52b1be79a4e55b79
François Poirotte <clicky@erebot.net>
2012-05-05 13:02:08 +0200

    Fix change_hook for buildbot 0.8.5.

--- src/change_hook.py
+++ src/change_hook.py
@@ -14,6 +14,7 @@ class CallableChangeHookResource(ChangeHookResource):
             raise ValueError("URI doesn't match change_hook regex: %s" % request.uri)
 
         changes = []
+        src = None
 
         # Was there a dialect provided?
         if uriRE.group(1):
@@ -24,12 +25,12 @@ class CallableChangeHookResource(ChangeHookResource):
         if dialect in self.dialects:
             if isinstance(self.dialects[dialect], object):
                 msg("Attempting to get changes using the %s hook" % dialect)
-                changes = self.dialects[dialect].getChanges(request)
+                res = self.dialects[dialect].getChanges(request)
             else:
                 msg("Attempting to load module buildbot.status.web.hooks." + dialect)
                 tempModule = namedModule('buildbot.status.web.hooks.' + dialect)
-                changes = tempModule.getChanges(request, self.dialects[dialect])
-            msg("Got the following changes %s" % changes)
+                res = tempModule.getChanges(request, self.dialects[dialect])
+            msg("Got the following changes %s" % (res, ))
 
         else:
             m = "The dialect specified, '%s', wasn't whitelisted in change_hook" % dialect
@@ -37,5 +38,11 @@ class CallableChangeHookResource(ChangeHookResource):
             msg("Note: if dialect is 'base' then it's possible your URL is malformed and we didn't regex it properly")
             raise ValueError(m)
 
+        # Recent buildmasters (>= 0.8.4 ?)
+        if isinstance(res, (tuple, list)) and isinstance(res[0], list) and \
+            res[0][0] and isinstance(res[0][0], dict):
+            return res
+
+        # Older buildmasters.
         return changes
 
