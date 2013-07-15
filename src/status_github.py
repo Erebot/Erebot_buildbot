8cd7b4ef7a4ca669aff2712422ddca7e474a5711
François Poirotte <clicky@erebot.net>
2013-07-15 12:34:12 +0200

    Remove compatibility with bb < 0.8.7.
    Also, add support for github pull requests and status notifications in
    the various status pages & hooks.

---
+++ src/status_github.py
@@ -0,0 +1,81 @@
+# -*- encoding: utf-8 -*-
+
+from twisted.internet import defer, reactor
+
+from buildbot.status.base import StatusReceiverBase
+from buildbot.status.result import Results, SUCCESS, WARNINGS, FAILURE, SKIPPED, EXCEPTION, RETRY
+from Erebot_buildbot.src.github_hook import gh_api
+
+class GithubStatus(StatusReceiverBase)
+    api_base = "https://api.github.com"
+
+    def __init__(self, token):
+        self._token = token
+
+    @defer.inlineCallbacks
+    def _get_last_change(self, bs):
+        sssid = bs.bsdict['sourcestampsetid']
+        yield ss = bs.master.db.sourcestamps.getSourceStamps(sssid)
+        when = None
+        last_ch = None
+        for sourcestamp in ss:
+            change = yield bs.master.db.changes.getChange(
+                            sourcestamp['changeids'])
+            if when is None or change['when_timestamp'] >= when:
+                when = change['when_timestamp']
+                last_ch = change
+        defer.returnValue(last_ch)
+
+    @defer.inlineCallbacks
+    def _submitted(self, bss):
+        last_ch = yield self._get_last_change(bss)
+        gh_api(
+            "%s/repos/%s/statuses/%s" % (
+                self.api_base,
+                last_ch['project'],
+                last_ch['revision'],
+            ),
+            self._token,
+            {
+                "state": "pending",
+                "target_url": "%scomponents?buildset=%d" % (
+                    misc.BUILDBOT_URL,
+                    bss.id,
+                ),
+                "description": "Buildbot is processing the change...",
+            }
+        )
+        def finished(buildset):
+            return reactor.callLater(0.1, self._finished, buildset)
+        bss.waitUntilFinished(finished)
+
+    @defer.inlineCallbacks
+    def _finished(self, bss):
+        last_ch = yield self._get_last_change(bss)
+        states = {
+            SUCCESS: 'success',
+            WARNINGS: 'success',
+            FAILURE: 'failure',
+        }
+        desc = "%s: %s" % (Results[bss.getResults()], bss.getReason())
+
+        gh_api(
+            "%s/repos/%s/statuses/%s" % (
+                self.api_base,
+                last_ch['project'],
+                last_ch['revision'],
+            ),
+            self._token,
+            {
+                "state": states.get(bss.getResults(), 'error'),
+                "target_url": "%scomponents?buildset=%d" % (
+                    misc.BUILDBOT_URL,
+                    bss.id,
+                ),
+                "description": desc,
+            }
+        )
+
+    def buildsetSubmitted(self, buildset):
+        reactor.callLater(0.1, self._submitted, buildset)
+
