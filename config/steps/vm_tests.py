84fd5339af7b65a6718f07b02c681ac4b3c11086
François Poirotte <clicky@erebot.net>
2013-08-04 16:01:08 +0200

    Wait for all tests to complete.

--- config/steps/vm_tests.py
+++ config/steps/vm_tests.py
@@ -4,16 +4,15 @@ from buildbot.process import factory
 from buildbot.process.properties import WithProperties
 from buildbot.steps import trigger
 from Erebot_buildbot.src import master
-from Erebot_buildbot.src.steps import MorphProperties
 from Erebot_buildbot.config.steps import common
 import secrets
 
 VM_TESTS = factory.BuildFactory()
 VM_TESTS.addStep(common.fill_properties)
 
-for buildslave in secrets.BUILDSLAVES:
-    VM_TESTS.addStep(trigger.Trigger(
-        schedulerNames=["Tests - %s" % buildslave],
-        copy_properties=['project', 'repository', 'branch', 'revision'],
-    ))
+VM_TESTS.addStep(trigger.Trigger(
+    schedulerNames=["Tests - %s" % bs for bs in secrets.BUILDSLAVES],
+    copy_properties=['project', 'repository', 'branch', 'revision'],
+    waitForFinish=True,
+))
 
